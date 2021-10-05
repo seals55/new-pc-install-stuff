a70071d6de3d866c2378e6670710f752d5c9c901
seals55 <seals55@users.noreply.github.com>
2021-10-05 00:21:33 -0400

    update

---
+++ choco_install.bat
@@ -0,0 +1,14 @@
+choco upgrade -y notepadplusplus googlechrome steam-client goggalaxy discord twitch sharex
+
+choco upgrade -y choco-cleaner winrar
+
+@rem failing crc
+@rem choco install -y origin 
+@rem choco install -y bethesdanet
+
+@rem choco install -y vscode
+
+choco install -y vmwareworkstation
+
+
+
