2c1675313395799592ebad24dcc7825c757c0eca
seals55 <seals55@users.noreply.github.com>
2021-10-06 19:15:04 -0400

    change to powershell

---
+++ choco_install.ps1
@@ -0,0 +1,38 @@
+choco upgrade -y `
+    notepadplusplus `
+    googlechrome `
+    steam-client `
+    goggalaxy `
+    discord `
+    twitch `
+    sharex `
+    choco-cleaner `
+    winrar `
+    mpc-hc `
+    directoryopus `
+    crystaldiskmark `
+    amd-ryzen-chipset `
+    cpu-z `
+    evernote `
+    gh `
+    gpu-z `
+    hwmonitor `
+    obs-studio `
+    powershell-core `
+    python `
+    cinebench `
+    msiafterburner `
+    hwinfo
+
+# failing crc
+# choco install -y origin
+# choco install -y bethesdanet
+
+# Not sure whats up with this one, says it works but no icon
+# choco install -y vscode
+
+# Initial install is fine, but let app upgrade itself.
+# choco install -y vmwareworkstation
+
+
+
